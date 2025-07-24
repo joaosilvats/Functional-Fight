a467421879e70b80899a64eae9ebb2d6169560be
joao.silva <joao.silva2601@outlook.com.br>
2025-07-23 22:03:09 -0300

    creating the doAttack function and implementing it in the start function

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -70,21 +70,55 @@ class Stage {
 
   start() {
     this.update();
+
+    this.fighter1El
+      .querySelector('.attack-button')
+      .addEventListener('click', (ev) =>
+        this.doAttack(this.fighter1, this.fighter2)
+      );
+
+    this.fighter2El
+      .querySelector('.attack-button')
+      .addEventListener('click', (ev) =>
+        this.doAttack(this.fighter2, this.fighter1)
+      );
   }
 
   update() {
     // fighter 1
-    this.fighter1El.querySelector(
-      '.name'
-    ).innerHTML = `${this.fighter1.name} ${this.fighter1.life} HP`;
+    this.fighter1El.querySelector('.name').innerHTML = `${
+      this.fighter1.name
+    } ${this.fighter1.life.toFixed(1)} HP`;
     let lifePct1 = (this.fighter1.life / this.fighter1.maxPowerLife) * 100;
     this.fighter1El.querySelector('.bar').style.width = `${lifePct1}%`;
 
     //fighter 2
-    this.fighter2El.querySelector(
-      '.name'
-    ).innerHTML = `${this.fighter2.name} ${this.fighter2.maxPowerLife}  HP`;
-    let lifePct2 = (this.fighter2.life / this.fighter2.life) * 100;
+    this.fighter2El.querySelector('.name').innerHTML = `${
+      this.fighter2.name
+    } ${this.fighter2.life.toFixed(1)} HP`;
+    let lifePct2 = (this.fighter2.life / this.fighter2.maxPowerLife) * 100;
     this.fighter2El.querySelector('.bar').style.width = `${lifePct2}%`;
+  }
+
+  doAttack(attacking, attacked) {
+    if (attacking.life <= 0 || attacked.life <= 0) {
+      console.log('cachorros mortos');
+      return;
+    }
+
+    let attackFactor = (Math.random() * 2).toFixed(2);
+    let defenseFactor = (Math.random() * 2).toFixed(2);
+
+    let actualAttack = attacking.strengthAttack * attackFactor;
+    let actualDefense = attacked.strengthDefense * defenseFactor;
+
+    if (actualAttack > actualDefense) {
+      console.log('tomou dano dog');
+      attacked.life -= actualAttack;
+    } else {
+      console.log(`${attacked.name} defendeu`);
+    }
+
+    this.update();
   }
 }
